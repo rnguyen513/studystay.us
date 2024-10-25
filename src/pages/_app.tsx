564bb4de15ad2bebf94258ceb96aa44a27beb979
Ryan Nguyen <rnguyen513@gmail.com>
2024-10-24 20:54:38 -0400

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>        
       </Head>
       <Component {...pageProps}/>
+      <Analytics/>
     </SessionProvider>
   )
 }
