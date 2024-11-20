bde3c60c780e948be570c9491576491698324c02
Ryan Nguyen <rnguyen513@gmail.com>
2024-11-20 17:51:48 -0500

    update api again

--- src/pages/api/pushtobot.ts
+++ src/pages/api/pushtobot.ts
@@ -5,27 +5,25 @@ export default async function handler(
   res: NextApiResponse
 ) {
     console.log(req.body);
-    if (!req.body.record) return res.status(500).send("No record");
+    if (!req.body.record) return res.status(400).send("No record");
 
     //TODO: check if from supabase otherwise quit
 
-    fetch("https://api.groupme.com/v3/bots/post", {
+    await fetch("https://api.groupme.com/v3/bots/post", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
             "text": `${req.body.record.title} posted by ${req.body.record.postedbyemail}`,
+            // "text": `${Math.random()}`,
             "bot_id": process.env.GROUPME_BOT_ID
         })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Success:", data);
-        return res.status(200).send("OK");
     })
     .catch(error => {
         console.error('Error:', error)
         return res.status(500).send(`Error: ${error}`);
     });
+
+    return res.status(200).send("Success");
 }
