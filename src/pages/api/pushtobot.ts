38350277a969c1aa8ef31ddd7e8d2315b54e8513
Ryan Nguyen <rnguyen513@gmail.com>
2024-11-20 18:31:37 -0500

    fix debug

--- src/pages/api/pushtobot.ts
+++ src/pages/api/pushtobot.ts
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    console.log("query: " + req.query + ", body: " + req.body + ", req: " + req);
+    console.log("query: " + req.query as string + ", body: " + req.body as string+ ", req: " + req as string);
     if (req.body.SUPABASE_ANON_KEY as string != process.env.SUPABASE_ANON_KEY as string) return res.status(401).send("Unauthorized");
     if (!req.body.record) return res.status(400).send("No record");
 
