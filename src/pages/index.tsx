5ec6eae5b4aa6b8e03f569713fd645078ef140c1
Ryan Nguyen <rnguyen513@gmail.com>
2025-07-13 21:37:30 -0400

    fix 'learn more' button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,7 +66,7 @@ export default function LandingPage() {
                             initial={{ opacity: 0, y: 20 }}
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.8, delay: 0.6 }}
-                            onClick={() => router.push("/about")}
+                            onClick={() => router.push("/in")}
                         >
                             Learn More
                         </motion.button>
